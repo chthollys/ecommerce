03ea19fd6990baad401e16d67904313db243197d
Raynanda128 <raynandaramadhan1@gmail.com>
2024-10-28 21:03:04 +0700

    Update register-page.php

--- register-page.php
+++ register-page.php
@@ -1,1 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Register</title>
+   <link rel="stylesheet" href="style.css">
 
+</head>
+<body>
+
+<?php
+session_start();
+if(isset($_SESSION['message'])){
+   echo '<div class="message" onclick="this.remove();">'.$_SESSION['message'].'</div>';
+   unset($_SESSION['message']);
+}
+?>
+
+<div class="form-container">
+   <form action="registerProcess.php" method="post">
+      <h3>Register Now</h3>
+      <input type="text" name="name" required placeholder="Enter username" class="box">
+      <input type="email" name="email" required placeholder="Enter email" class="box">
+      <input type="password" name="password" required placeholder="Enter password" class="box">
+      <input type="password" name="cpassword" required placeholder="Confirm password" class="box">
+      <input type="submit" name="submit" class="btn" value="Register Now">
+      <p>Already have an account? <a href="login-page.php">Login now</a></p>
+   </form>
+</div>
+
+</body>
+</html>
