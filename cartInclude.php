9fc783fb3f12d02328a8d122def51f914f280772
chthollys <jason130105@gmail.com>
2024-11-24 09:42:06 +0700

    include cartInclude.php

---
+++ cartInclude.php
@@ -0,0 +1,41 @@
+<?php
+
+// Create connection
+$conn = mysqli_connect('localhost', 'root', '', 'e-commerce_db');
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if (isset($_POST['status'])) {
+        $status = (int)$_POST['status'];
+        echo "Status: " . $status;
+    } else {
+        echo "Status not set in form submission.";
+    }
+
+    // echo "<pre>";
+    // print_r($_POST); // Check if 'status' and 'product_id' are submitted
+    // echo "</pre>";
+    $user_id = $_POST['user_id'];
+    $product_id = $_POST['product_id'];
+
+    $stmt = mysqli_prepare($conn, "UPDATE cart SET status = ? WHERE user_id = ? AND product_id = ? ");
+    mysqli_stmt_bind_param($stmt, 'iii', $status, $user_id, $product_id);
+    if ($stmt->execute()) {
+        echo "Status updated successfully.";
+        header('Location: cart.php');
+    } else {
+        echo "Failed to update status: " . $stmt->error;
+    }
+
+    // Close the statement and connection
+    mysqli_stmt_close($stmt);
+
+} else {
+    echo "Server method can't be detected";
+}
+mysqli_close($conn);
+exit();
+?>
